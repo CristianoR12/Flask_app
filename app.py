1ab9b4bda69839aa2c96c7fb934c09dfbd5842e5
Cristiano Rausse <cristiano.rausse@gmail.com>
2021-06-21 16:14:23 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, redirect, request
+import mysql.connector
+
+app = Flask(__name__)
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="Psw_123456#!",
+  database="new_db_test",
+  auth_plugin='mysql_native_password'
+)
+
+db = mydb.cursor() 
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/register", methods=["POST"])
+def register():
+    firstname = request.form.get("firstname")
+    if not firstname:
+        return render_template("error.html", message="Missing firstname")
+
+    lastname = request.form.get("lastname")
+    if not lastname:
+        return render_template("error.html", message="Missing lastname")
+
+    email = request.form.get("email")
+    if not email:
+        return render_template("error.txt", message="Invalid email")
+
+    #Database to store the data of the new guests
+    sql = "INSERT INTO MyGuests (firstname, lastname, email) VALUES (%s, %s, %s)"
+    val = (firstname, lastname, email)
+    db.execute(sql, val)
+    mydb.commit()
+
+    return redirect("/guests")    
+
+@app.route("/guests", methods=["POST", "GET"])
+def guests():
+  db.execute("SELECT * FROM MyGuests")
+  row = db.fetchall()
+  
+  return render_template("guests.html", guests=row)
+
+ 
